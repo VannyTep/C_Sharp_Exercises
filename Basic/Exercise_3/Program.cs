ca402ae9ed576d8d67e1f2195e7990c2e6af544c
TepChoulong <tepchoulongzin@gmail.com>
2023-07-12 21:47:09 +0700

    First Commit

---
+++ Basic/Exercise_3/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+    Write a C# Sharp program to check whether a given number is positive or negative.
+    ------------------------------------
+    Test Data : 
+        14, -2, 20, -18, 4-(-4)
+    ------------------------------------
+    Expected Output :
+        14 is a positive number
+*/
