8baed2f9c4ba96a8677df55f2861d0830a22d01c
VannyTep <89964001+VannyTep@users.noreply.github.com>
2023-07-13 20:56:05 +0700

    exercise 1 done

--- Basic/Exercise_1/Program.cs
+++ Basic/Exercise_1/Program.cs
@@ -10,3 +10,21 @@
         First Number : 6
         Second Number : 5
 */
+int num1;
+int num2;
+
+
+Console.Write("Enter your first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter your second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(""); // Space 
+
+Console.WriteLine("After Swapping, First Number : " + num2);
+
+Console.WriteLine("After Swapping, Second Number : " + num1);
+
+
+//13.07.2023//
